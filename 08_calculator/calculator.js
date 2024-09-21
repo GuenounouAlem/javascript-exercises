fbbe763934670d8608c6494c01eecb64480e780e
GuenounouAlem <44892245+GuenounouAlem@users.noreply.github.com>
2024-09-21 19:14:18 +0100

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return +num1 + +num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return +num1 - +num2   
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length) {
+    let sum = array.reduce((total, value) => {
+      return total + value
+    })
+    return sum
+  } else {
+    return 0
+  }
+  
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length) {
+    let sum = array.reduce((total, value) => {
+      return total * value
+    })
+    return sum
+  } else {
+    return 0
+  }
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num <= 1) {
+    return 1
+  }
+  return num * factorial(num - 1)
 };
 
 // Do not edit below this line
