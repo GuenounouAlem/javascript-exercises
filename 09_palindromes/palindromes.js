adefb1b2ec78064ea28776fc90e72c3cb1892a53
GuenounouAlem <123456789+slayershad05@gmail.com>
2024-09-22 13:19:36 +0100

    shitty 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let alpha = str
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphanumerical.includes(char))
+    .join('')
 
+    let revAlpha = alpha.split('').reverse().join('')
+
+    return alpha === revAlpha    
 };
 
 // Do not edit below this line
