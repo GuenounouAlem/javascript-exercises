d4ebd9865675b3ab1581d2de581c7f291e7f73e8
GuenounouAlem <44892245+GuenounouAlem@users.noreply.github.com>
2024-09-23 00:06:40 +0100

    12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear()
+    let oldest = people.reduce((old, nextPerson) => {
+        if (!nextPerson.yearOfDeath) {
+            nextPerson.yearOfDeath = thisYear
+            
+        }
+        if (!old.yearOfDeath) {
+            old.yearOfDeath = thisYear
+        }
+        let nextAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth
+        let oldAge = old.yearOfDeath - old.yearOfBirth
 
+
+        return oldAge < nextAge ? nextPerson : old
+    })
+    return oldest
 };
 
 // Do not edit below this line
